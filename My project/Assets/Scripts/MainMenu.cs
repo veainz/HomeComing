f961221ae47d0196b5039e524e20c1d2a06df43e
Nguyễn Chước Long <longmoc93@gmail.com>
2025-01-13 11:58:54 +0700

    1

---
+++ My project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+public class MainMenu : MonoBehaviour
+{
+
+   [SerializeField] GameObject PauseMenu;
+   [SerializeField] GameObject GameOverMenu;
+
+   public void Playgame()
+   {
+      SceneManager.LoadSceneAsync(1);
+   }
+   public void QuitGame()
+   {
+      Application.Quit();
+
+#if UNITY_EDITOR
+      EditorApplication.isPlaying = false;
+#endif
+   }
+
+   public void PauseWithPauseMenu()
+   {
+      PauseMenu.SetActive(true);
+      Time.timeScale = 0;
+   }
+   public void ExitWithPauseMenu()
+   {
+      SceneManager.LoadScene("MainMenu");
+      Time.timeScale = 1;
+
+   }
+   public void RestartWithPauseMenu()
+   {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+      Time.timeScale = 1;
+
+   }
+   public void ReseumWithPauseMenu()
+   {
+      PauseMenu.SetActive(false);
+      Time.timeScale = 1;
+   }
+   public void GameOver()
+   {
+      GameOverMenu.SetActive(true);
+     
+   }
+   public void PlayAgain()
+   {
+       Debug.Log("Restarting game...");
+      GameOverMenu.SetActive(false);
+   
+      SceneManager.LoadScene("MainGame");;
+   }
+   
+
+
+}
