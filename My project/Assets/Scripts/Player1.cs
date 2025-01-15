5b2eb510eeadcdda9cc6da86dc4d7bf1527f7b7e
Nguyễn Chước Long <longmoc93@gmail.com>
2025-01-15 13:51:19 +0700

    3

--- My project/Assets/Scripts/Player1.cs
+++ My project/Assets/Scripts/Player1.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
-
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
 public class Player1 : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float rollBoost = 5f;
     public float RollTime;
+
     private float rollTime;
     private bool rollOnce = false;
 
@@ -18,7 +21,14 @@ public class Player1 : MonoBehaviour
     // Biến cho máu
     public int maxHealth = 100;
     private int currentHealth;
+    private bool ishealing = false;
+    private GameObject healingInstance; // Biến lưu đối tượng healing
+    private Animator healingAnimator;
+    public Transform healPoss;
 
+    public float healTime = 5f;
+    public int heal = 30;
+    public GameObject healing; 
     // Thêm một biến cho Collider2D
     private Collider2D playerCollider;
     public MainMenu GameOver;
@@ -35,7 +45,6 @@ public class Player1 : MonoBehaviour
             healthBar.UpdateBar(currentHealth, maxHealth);
         }
     }
-
     private void Update()
     {
         moveInput.x = Input.GetAxis("Horizontal");
@@ -75,6 +84,33 @@ public class Player1 : MonoBehaviour
         {
             transform.localScale = new Vector3(moveInput.x > 0 ? 1 : -1, 1, 0);
         }
+        StartCoroutine(Healing());
+    }
+     private IEnumerator Healing()
+    {
+        while (true)
+        {
+            if (currentHealth < maxHealth && !ishealing)
+            {
+                ishealing = true;
+                yield return new WaitForSeconds(healTime);
+                if (currentHealth < maxHealth)
+                {
+                    StartHealing();
+                    currentHealth += heal;
+                    Debug.Log("Current Health: " + currentHealth);   
+                    if (currentHealth >= maxHealth)
+                    {  
+                        Debug.Log("Reached max health");
+                        StopHealing();
+                        currentHealth = maxHealth;
+                    }
+                    healthBar.UpdateBar(currentHealth, maxHealth);
+                }
+                ishealing = false;
+            }
+            yield return null;
+        }
     }
 
     public void TakeDamage(int damage)
@@ -95,7 +131,7 @@ public class Player1 : MonoBehaviour
         if (currentHealth <= 0)
         {
             Die();
-            GameOver.GameOver();
+            // GameOver.GameOver();
         }
     }
 
@@ -109,5 +145,31 @@ public class Player1 : MonoBehaviour
     public int GetCurrentHealth()
     {
         return currentHealth;
+    }
+
+     private void StartHealing()
+    {
+        // Nếu healingInstance chưa được tạo, tạo mới healing
+        if (healingInstance == null)
+        {
+            healingInstance = Instantiate(healing, healPoss.position, Quaternion.identity);
+            healingInstance.transform.SetParent(transform);
+            healingAnimator = healingInstance.GetComponent<Animator>(); // Lưu animator cho đối tượng healing
+            healingAnimator.SetBool("isHealing", true);
+        }
+    }
+
+    private void StopHealing()
+    {
+        if (healingAnimator != null)
+        {
+            healingAnimator.SetBool("isHealing", false);
+            Destroy(healingInstance, 1f); // Thời gian chờ để hủy healingInstance sau khi dừng hoạt ảnh
+            healingInstance = null; // Reset lại healingInstance
+        }
+        else
+        {
+            Debug.LogError("healingAnimator is null, cannot stop healing.");
+        }
     }
 }
